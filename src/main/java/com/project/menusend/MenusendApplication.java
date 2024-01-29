a873ab241ad8d250b6413a8d3721a7ed875251a8
JianChoi-Kor <wldks79@gmail.com>
2024-01-29 22:01:47 +0900

    init

---
+++ src/main/java/com/project/menusend/MenusendApplication.java
@@ -0,0 +1,13 @@
+package com.project.menusend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenusendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenusendApplication.class, args);
+	}
+
+}
